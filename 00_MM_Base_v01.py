82176a5549cbc993b3782d17d6ba55f1a5eddb93
LeenaIm <99370611+LeenaIm@users.noreply.github.com>
2023-02-20 11:04:09 +1300

    Add files via upload

--- 00_MM_Base_v01.py
+++ 00_MM_Base_v01.py
@@ -1,7 +1,5 @@
 # Functions go here
 
-
-
 # Checks user has entered yes / no to a question
 def yes_no (question) :
 
@@ -34,7 +32,39 @@ def not_blank(question):
         else:
             return response
 
+# checks users enter an integer to a given question
+def num_check(question):
+
+    while True:
+
+        try:
+            response = int(input(question))
+            return response
+
+        except ValueError:
+            print("Please enter an integer.")
+
+
 # main routine starts here
+
+# Calculate the ticket price based on the age
+def calc_ticket_price(var_age):
+
+    # ticket is $7.50 for users under 16
+    if var_age < 16:
+        price = 7.5
+
+    # ticket is $10.50 for users between 16 and 64
+    elif var_age < 65:
+        price = 10.5
+
+    # ticket price is $6.50 for seniors (65+)
+    else:
+        price = 6.5
+
+    return price
+
+
 
 # set maximum number of tickets below
 MAX_TICKETS = 3
@@ -47,12 +77,26 @@ if want_instructions == "yes":
     print("Instructions go here")
 
 # loop to sell tickets
-
 while tickets_sold < MAX_TICKETS:
     name = not_blank("Enter your name (or 'xxx' to quit) ")
 
     if name == 'xxx':
         break
+
+    age = num_check("Age: ")
+
+    if 12 <= age <= 120:
+        pass
+    elif age < 12:
+        print("Sorry you are too young for this movie")
+        continue
+    else:
+        print("That looks like a typo, please try again")
+        continue
+
+    # calculate ticket cost
+    ticket_cost = calc_ticket_price(age)
+    print("Age: {}, Ticket Price: ${:.2f}".format(age, ticket_cost))
 
     tickets_sold += 1
 
