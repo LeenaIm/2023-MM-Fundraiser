a385d25143a4390fcc2d5c0206beca14f714fb15
LeenaIm <99370611+LeenaIm@users.noreply.github.com>
2023-02-27 10:28:56 +1300

    Add files via upload

--- 07_string_checker_v2.py
+++ 07_string_checker_v2.py
@@ -1,24 +1,20 @@
 # checks that users enter a valid response (eg yes / no)
 # cash / credit based on a list of options
 def string_checker(question, num_letters, valid_responses):
-    error = "Please choose {} or {}".format(valid_responses[0], valid_responses[1])
-
-    if num_letters == 1:
-        short_version = 1
-    else:
-        short_version = 2
+    error = "Please choose {} or {}".format(valid_responses[0],
+                                            valid_responses[1])
 
     while True:
         response = input(question).lower()
 
         for item in valid_responses:
-            if response == item[:short_version] or response == item:
+            if response == item[:num_letters] or response == item:
                 return item
 
             print(error)
 
 
-# main routine goes here  
+# main routine goes here
 yes_no_list = ["yes", "no"]
 payment_list = ["cash", "credit"]
 
